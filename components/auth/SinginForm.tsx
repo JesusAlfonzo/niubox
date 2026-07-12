7d4ec7fefab2c8783f8ed1be450caabcf2f00401
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-07-12 18:29:25 -0400

    feat: añadir formularios de autenticación
    - Implementar páginas de inicio de sesión y registro
    - Integrar react-hook-form con componentes de Radix UI
    - Añadir lógica de validación para las credenciales de usuario

--- components/auth/SinginForm.tsx
+++ components/auth/SinginForm.tsx
@@ -1,31 +1,109 @@
-"use client"
+"use client";
 
-import { Flex, TextField, Button } from "@radix-ui/themes";
+import { Flex, TextField, Button, Text } from "@radix-ui/themes";
 import { EnvelopeClosedIcon, LockClosedIcon } from "@radix-ui/react-icons";
+import { useForm, Controller } from "react-hook-form";
 
 export default function SinginForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("Datos del formulario:", data);
+  });
+
   return (
-    <Flex direction="column" gap="2">
-      <label htmlFor="email">Email</label>
-      <TextField.Root
-        type="email"
-        placeholder="example@email.com"
-        radius="large"
-        autoFocus
-      >
-        <TextField.Slot>
-          <EnvelopeClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+    <form onSubmit={onSubmit}>
+      <Flex direction="column" gap="2">
+        {/* Campo Email */}
+        <label htmlFor="email">
+          <Text as="div" size="2" mb="1" weight="bold">
+            Email
+          </Text>
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: {
+                message: "Email is Required",
+                value: true,
+              },
+            }}
+            render={({ field }) => (
+              <TextField.Root
+                id="email"
+                type="email"
+                placeholder="example@email.com"
+                radius="large"
+                autoFocus
+                {...field}
+              >
+                <TextField.Slot>
+                  <EnvelopeClosedIcon height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            )}
+          />
+        </label>
 
-      <label htmlFor="password">Password</label>
-      <TextField.Root type="password" placeholder="********" radius="large">
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        {errors.email && (
+          <Text size="1" color="red">
+            {errors.email.message}
+          </Text>
+        )}
 
-      <Button>Sing In</Button>
-    </Flex>
+        {/* Campo Password */}
+        <label htmlFor="password">
+          <Text as="div" size="2" mb="1" weight="bold">
+            Password
+          </Text>
+          <Controller
+            name="password"
+            control={control}
+            rules={{
+              required: {
+                message: "Password is Required",
+                value: true,
+              },
+              minLength: {
+                message: "Password must be at least 8 characters",
+                value: 8,
+              },
+            }}
+            render={({ field }) => (
+              <TextField.Root
+                id="password"
+                type="password"
+                placeholder="********"
+                radius="large"
+                {...field}
+              >
+                <TextField.Slot>
+                  <LockClosedIcon height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            )}
+          />
+        </label>
+
+        {errors.password && (
+          <Text size="1" color="red">
+            {errors.password.message}
+          </Text>
+        )}
+
+        <Button type="submit" mt="4">
+          Sign In
+        </Button>
+      </Flex>
+    </form>
   );
 }
--- components/auth/SingupForm.tsx
+++ components/auth/SingupForm.tsx
@@ -1,51 +1,174 @@
 "use client";
-import { Flex, TextField, Button } from "@radix-ui/themes";
+import { Flex, TextField, Button, Text } from "@radix-ui/themes";
 import {
   EnvelopeClosedIcon,
   LockClosedIcon,
   PersonIcon,
 } from "@radix-ui/react-icons";
+import { useForm, Controller } from "react-hook-form";
 
-export default function RegisterForm() {
+export default function SingupForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("Datos del formulario:", data);
+  });
+
   return (
-    <Flex direction="column" gap="2">
-      <label htmlFor="email">Name:</label>
-      <TextField.Root
-        type="email"
-        placeholder="Your name"
-        radius="large"
-      >
-        <TextField.Slot>
-          <PersonIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+    <form onSubmit={onSubmit}>
+      <Flex direction="column" gap="2">
+        {/* Campo Name */}
+        <label htmlFor="name">
+          <Text as="div" size="2" mb="1" weight="bold">
+            Name:
+          </Text>
+          <Controller
+            name="name"
+            control={control}
+            rules={{
+              required: {
+                message: "Name is Required",
+                value: true,
+              },
+            }}
+            render={({ field }) => (
+              <TextField.Root
+                type="text"
+                placeholder="Your name"
+                radius="large"
+                autoFocus
+                {...field}
+              >
+                <TextField.Slot>
+                  <PersonIcon height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            )}
+          />
+        </label>
 
-      <label htmlFor="email">Email:</label>
-      <TextField.Root
-        type="email"
-        placeholder="example@email.com"
-        radius="large"
-      >
-        <TextField.Slot>
-          <EnvelopeClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        {errors.name && (
+          <Text size="1" color="red">
+            {errors.name.message}
+          </Text>
+        )}
 
-      <label htmlFor="password">Password:</label>
-      <TextField.Root type="password" placeholder="********" radius="large">
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        {/* Campo Email */}
+        <label htmlFor="email">
+          <Text as="div" size="2" mb="1" weight="bold">
+            Email:
+          </Text>
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: {
+                message: "Email is Required",
+                value: true,
+              },
+            }}
+            render={({ field }) => (
+              <TextField.Root
+                type="email"
+                placeholder="example@email.com"
+                radius="large"
+                {...field}
+              >
+                <TextField.Slot>
+                  <EnvelopeClosedIcon height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            )}
+          />
+        </label>
 
-      <label htmlFor="password">Confirm Password:</label>
-      <TextField.Root type="password" placeholder="********" radius="large">
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
+        {errors.email && (
+          <Text size="1" color="red">
+            {errors.email.message}
+          </Text>
+        )}
 
-      <Button>Sing Up</Button>
-    </Flex>
+        <label htmlFor="password">
+          <Text as="div" size="2" mb="1" weight="bold">
+            Password:
+          </Text>
+          <Controller
+            name="password"
+            control={control}
+            rules={{
+              required: {
+                message: "Password is Required",
+                value: true,
+              },
+            }}
+            render={({ field }) => (
+              <TextField.Root
+                type="password"
+                placeholder="********"
+                radius="large"
+                {...field}
+              >
+                <TextField.Slot>
+                  <LockClosedIcon height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            )}
+          />
+        </label>
+
+        {errors.password && (
+          <Text size="1" color="red">
+            {errors.password.message}
+          </Text>
+        )}
+
+        <label htmlFor="Password">
+          <Text as="div" size="2" mb="1" weight="bold">
+            Confirm Password:
+          </Text>
+          <Controller
+            name="confirmPassword"
+            control={control}
+            rules={{
+              required: {
+                message: "Confirm Password is Required",
+                value: true,
+              },
+            }}
+            render={({ field }) => (
+              <TextField.Root
+                type="password"
+                placeholder="********"
+                radius="large"
+                {...field}
+              >
+                <TextField.Slot>
+                  <LockClosedIcon height="16" width="16" />
+                </TextField.Slot>
+              </TextField.Root>
+            )}
+          />
+        </label>
+
+        {errors.confirmPassword && (
+          <Text size="1" color="red">
+            {errors.confirmPassword.message}
+          </Text>
+        )}
+
+        <Button>Sing Up</Button>
+      </Flex>
+    </form>
   );
 }
