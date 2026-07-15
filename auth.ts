0297acd1cd5f6a893dd04bbf1848e64b313d8121
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-07-15 11:06:16 -0400

    refactor: migrar de next-auth a better-auth
    Cambios en package.json y package-lock.json.
    
    Eliminación de archivos de configuración antiguos (ej. auth.ts de NextAuth).
    
    Adición de los nuevos archivos de better-auth.

--- auth.ts
+++ auth.ts
@@ -1,30 +1,14 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
+import { saltAndHashPassword } from "@/utils/password"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
+  providers:[
     Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        // 1. Aquí recibes los datos del formulario
-        const email = credentials.email as string;
-        const password = credentials.password as string;
-
-        // 2. Aquí llamarías a tu base de datos (PostgreSQL/MySQL)
-        // const user = await db.user.findUnique({ where: { email } });
-        
-        // 3. Validación (simulada)
-        // if (!user || !bcrypt.compareSync(password, user.password)) {
-        //    return null; // El login falla
-        // }
-
-        // 4. Si todo está bien, devuelve el objeto del usuario
-        return { id: "1", name: "Usuario", email };
+      credentials:{
+        email: {label: "Email", type: "email", placeholder: "example@example.com"},
+        password: {label: "Password", type: "password", placeholder: "********"},
       }
-    }),
-  ],
-});
+    })
+  ]
+})
