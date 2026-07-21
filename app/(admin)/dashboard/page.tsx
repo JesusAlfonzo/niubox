97eb8c9779816e7467547d9a51f9c9705fe81ea3
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-07-21 13:45:43 -0400

    feat(dashboard): crear vista principal mostrando la sesion del usuario

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,8 +1,17 @@
-export default function DashboardPage() {
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import SignoutButton from "@/components/auth/SignoutButton";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <div>
-      <h1>Dashborad</h1>
-      <p>Bienvenido a niubox</p>
+      <h1>Dashboard</h1>
+      <p>Bienvenido a Niubox, {session?.user.name}</p>
+      <SignoutButton></SignoutButton>
     </div>
   );
 }
